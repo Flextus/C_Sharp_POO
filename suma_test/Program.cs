f0cbb802e00bdb2deedb489ccf2a70b8ede7f209
Flextus <benjasg10@gmail.com>
2024-09-23 14:58:54 -0600

    001_Benjamin_2309
    suma de c# e intro

---
+++ suma_test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Suma
+{
+    public static void Main(string[] args)
+    {
+        int a;
+        int b;
+        int suma;
+        Console.WriteLine("Escribe el primer numero\n");
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+
+        suma = a + b;
+        Console.WriteLine("La suma es: {0}", suma);
+    }
+}
