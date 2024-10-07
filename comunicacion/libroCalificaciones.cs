0ef79512db8a778b5010d493a90f02e8635356b1
Flextus <benjasg10@gmail.com>
2024-10-07 15:01:56 -0600

    001_Benjamin_0710
    clasehoy

---
+++ comunicacion/libroCalificaciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class LibroCalificaciones
+{
+    private string nombreCurso;
+
+    public string NombreCurso
+    {
+        get
+        {
+            return nombreCurso;
+        }
+        set
+        {
+            nombreCurso = value;
+        }
+    }
+
+    public void MostrarMensaje()
+    {
+        Console.WriteLine("Bienvenido al curso de \n{0}", NombreCurso);
+    }
+}
---
+++ comunicacion/libroPruebaDeCalificaciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class LibroPruebaDeCalificaciones
+{
+    public static void Main(string[] args)
+    {
+        LibroCalificaciones Libro1 = new LibroCalificaciones();
+        //Muestra el valor inicial si esta asignado
+        Console.WriteLine("El nombre inicial del Curso es: '{0}' \n ", Libro1.NombreCurso);
+
+
+        //Pedimos la emtrada del Usuario
+        Console.WriteLine("Por Favor escribe el nombre del curso");
+        string nombre = Console.ReadLine();
+
+        //Guardamos el nombre en la otra clase
+        Libro1.NombreCurso = nombre;
+        
+        //Muestra el mensjae de Bienvenida despues de especificar tu nombre
+        Libro1.MostrarMensaje();
+
+    }
+}
