04e364effae68fc286275854aa1b818f16a03bb3
Flextus <benjasg10@gmail.com>
2024-10-09 15:16:48 -0600

    001_Bnejamin_0910
    sistema bancario 01

---
+++ SistemaBancario/Cuenta.cs
@@ -0,0 +1,37 @@
+﻿//establecer un saldo en una cuenta bancaria
+using System;
+
+public class Cuenta
+{
+    private decimal saldo; //variable de instancia que almacenara el saldo de la cuenta
+                           // una instancia es una variable vacia que espera el dato
+                           // de el constructor interno
+
+    //contructor interno que rellena la instancia
+    public Cuenta(decimal saldoInicial)
+    {
+        Saldo = saldoInicial;
+    }
+
+    //acreditar un monto en la cuenta
+
+    public void Acredita(decimal monto)
+    {
+        Saldo = Saldo + monto; //suma monto al saldo
+    }
+
+    public decimal Saldo
+    {
+        get
+        {
+            return saldo;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                saldo = value;
+            }
+        }
+    }
+}
---
+++ SistemaBancario/PruebaCuenta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+
+public class PruebaCuenta
+{
+    public static void Main(string[] args)
+    {
+        Cuenta cuenta1 = new Cuenta(50.00M);//constructor 1er objeto
+        Cuenta cuenta2 = new Cuenta(-7.53M);//constructor 2do objeto
+        //la "M" es para decimales tal que "f" es para float
+
+        Console.WriteLine("Saldo de la Primer Cuenta: {0:C}", cuenta1.Saldo);
+        Console.WriteLine("Saldo de la Segunda Cuenta: {0:C}", cuenta2.Saldo);
+        //al hacer el ":C" declaramos que el dato lo queremos en formato de dinero
+
+        decimal montoDepositado;//deposita la cantidad indicada por el usuario
+
+        //solicita la usuario el monto a depositar
+        Console.WriteLine("Escriba el monto a depositar a la cuenta 1: ");
+        montoDepositado = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Se agregaron {0 :C} al saldo de la Cuenta 1", montoDepositado);
+        cuenta1.Acredita(montoDepositado);
+        Console.WriteLine("Escriba el monto a depositar a la cuenta 2: ");
+        montoDepositado = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Se agregaron {0 :C} al saldo de la Cuenta 2", montoDepositado);
+        cuenta2.Acredita(montoDepositado);
+
+        //muestra el saldo actualizado
+        Console.WriteLine("El saldo de la Cuenta 1 es: {0:C}", cuenta1.Saldo);
+        Console.WriteLine("El saldo de la Cuenta 2 es: {0:C}", cuenta2.Saldo);
+
+    }
+
+}
